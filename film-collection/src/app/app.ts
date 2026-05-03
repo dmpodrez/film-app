06f90986d6db611c262ed8cd1710b9facca26dca
Dmitry Podrez <dima.padrez.00@icloud.com>
2026-05-03 21:30:57 +0200

    feat: add film search and favorites

--- film-collection/src/app/app.ts
+++ film-collection/src/app/app.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { films } from './data/films';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
 })
 export class App {
   films = films;
+
+  search = '';
+
+  get filteredFilms() {
+    return this.films.filter(film =>
+      film.title.toLowerCase().includes(this.search.toLowerCase())
+    );
+  }
+
+  toggleFavorite(id: number) {
+    const film = this.films.find(film => film.id === id);
+
+    if (film) {
+      film.favorite = !film.favorite;
+    }
+  }
 }
