8d383b97b5e12167f0ec0d8132387e47372741d1
Dmitry Podrez <dima.padrez.00@icloud.com>
2026-05-04 18:04:49 +0200

    feat: add autofocus directive style: improve film collection layout

--- film-collection/src/app/pages/home/home.ts
+++ film-collection/src/app/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FilmList } from '../../components/film-list/film-list';
 import { FilmService } from '../../services/film';
+import { Autofocus } from '../../directives/autofocus';
 
 @Component({
   selector: 'app-home',
-  imports: [FormsModule, FilmList],
+  imports: [FormsModule, FilmList, Autofocus],
   templateUrl: './home.html'
 })
 export class Home {
